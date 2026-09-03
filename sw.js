0c45f0247f5caef5f5fc3a8e694dcfb00ccc3896
MHK-prog <mohammadhoseinkhodai@gmail.com>
2026-09-03 19:44:31 +0330

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,19 +1,44 @@
-const CACHE_NAME = 'class-grades-v1';
-const ASSETS = [
-  './',
-  './index.html',
-  './manifest.json',
-  'https://cdn.jsdelivr.net/npm/html2canvas@1.4.1/dist/html2canvas.min.js'
+const CACHE_NAME = 'class-manager-v1';
+const urlsToCache = [
+  '/class-manager/',
+  '/class-manager/index.html',
+  '/class-manager/manifest.json',
+  '/class-manager/html2canvas.min.js',
+  '/class-manager/icon-192.png',
+  '/class-manager/icon-512.png'
 ];
 
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+// Install Service Worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
   );
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((res) => res || fetch(e.request))
+// Activate Service Worker
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Fetch resources
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
   );
 });
